02879196e568c53c8a3246b92a70067fccabda1f
R0ckStrongo <knut9000@gmail.com>
2023-12-14 16:50:06 +0100

    bug fixes and small improvments on all sites

--- assets/js/scrollanim.js
+++ assets/js/scrollanim.js
@@ -8,3 +8,15 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function refreshPage () {
+	var page_y = document.getElementsByTagName("body")[0].scrollTop;
+	window.location.href = window.location.href.split('?')[0] + '?page_y=' + page_y;
+}
+window.onload = function () {
+	setTimeout(refreshPage, 35000);
+	if ( window.location.href.indexOf('page_y') != -1 ) {
+		var match = window.location.href.split('?')[1].split("&")[0].split("=");
+		document.getElementsByTagName("body")[0].scrollTop = match[1];
+	}
+}
