609d4a030e557e135898f103140aa0579b70534f
R0ckStrongo <knut9000@gmail.com>
2026-02-19 20:37:29 +0100

    adjusted loading

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,6 +1,6 @@
 /* Loading animation */
 document.onreadystatechange = function () {
-	 if (document.readyState !== "interactive" || document.readyState !== "complete") {
+	 if (document.readyState !== "complete") {
 		document.querySelector("body").style.visibility = "hidden";
 		document.getElementById("loading_screen").style.visibility = "visible";
 	 } else {
