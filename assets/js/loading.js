47035dcb27ce8aed80f5ea49ea0bf813c306fb9e
R0ckStrongo <knut9000@gmail.com>
2026-02-27 16:20:07 +0100

    smaller slideshow, prepared for hero video

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,18 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const heroImg = document.querySelector("#hero-image");
-  const loader = document.getElementById("loading_screen");
-
-  function hideLoader() {
-    loader.classList.add("fadeout");
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 500);
-  }
-
-  // Falls Bild schon aus Cache geladen ist
-  if (heroImg.complete && heroImg.naturalWidth !== 0) {
-    hideLoader();
-  } else {
-    heroImg.addEventListener("load", hideLoader);
-  }
+document.addEventListener("DOMContentLoaded", function () { 
+  const heroImg = document.querySelector("#hero-image"); 
+  const loader = document.getElementById("loading_screen"); 
+  
+  function hideLoader() { 
+    loader.classList.add("fadeout"); 
+    setTimeout(() => { loader.style.display = "none"; }, 500); } 
+  // Falls Bild schon aus Cache geladen ist 
+  if (heroImg.complete && heroImg.naturalWidth !== 0) { 
+    hideLoader(); } 
+  else { heroImg.addEventListener("load", hideLoader); } 
 });
